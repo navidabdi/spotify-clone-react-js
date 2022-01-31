130afec0748456e666351aa3b80d2327013ad552
navidabdi <artsanaat@gmail.com>
2022-01-31 23:03:52 +0330

    authorization with jwt

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Sidebar from '../components/Sidebar'
 
 export default function Home() {
+  // console.log(process.env.NEXT_PUBLIC_CLIENT_SECRET)
   return (
     <div className="h-screen overflow-hidden bg-black">
       <main>
---
+++ pages/login.js
@@ -0,0 +1,32 @@
+import { getProviders, signIn } from 'next-auth/react'
+
+const login = ({ providers }) => {
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-black">
+      <img className="mb-5 w-52" src="https://links.papareact.com/9xl" alt="" />
+
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+            className="rounded-lg bg-[#18d860] p-5 text-white"
+          >
+            Login With {provider.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default login
+
+export async function getServerSideProps() {
+  const providers = await getProviders()
+
+  return {
+    props: {
+      providers,
+    },
+  }
+}
