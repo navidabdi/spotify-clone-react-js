749187c3c021797c42e6eb4e8de5a30960323f05
navidabdi <artsanaat@gmail.com>
2022-02-01 10:10:42 +0330

    redirect user if they are not loged in

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,10 +7,20 @@ import {
   RssIcon,
 } from '@heroicons/react/outline'
 
+import { signOut, useSession } from 'next-auth/react'
+
 const Sidebar = () => {
+  // const { data: session, status } = useSession()
+  // console.log(session)
   return (
     <div className="border-r border-gray-900 p-5 text-sm text-gray-500">
       <div className="space-y-4">
+        <button
+          onClick={() => signOut()}
+          className="flex items-center space-x-2 hover:text-white"
+        >
+          <p>Logout</p>
+        </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
